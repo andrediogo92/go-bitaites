6cdd8b87e9e916d122f541a218e68b7d90843130
André Diogo <redan.blue27@gmail.com>
2018-05-27 00:38:14 +0100

    Adds Ids, Feed and Posts operations.
    Adds some singleton variables and their initialization.
    
    Ids are cryptographic ECDSA keys.
    
    Feeds will just contain hashes of the actual posts.
    
    Posts are the feed abstraction (id + post pointers).

--- db/badgeradapt.go
+++ db/badgeradapt.go
@@ -1,23 +1,35 @@
 package db
 
 import (
-	"log"
-	"math/rand"
+"bytes"
+"encoding/gob"
+"log"
+"math/rand"
+	"time"
 
 	"../timeline"
-	"github.com/dgraph-io/badger"
+"github.com/Seriyin/go-bitaites/peer"
+"github.com/dgraph-io/badger"
+	"github.com/pkg/errors"
 )
+
 
 type DBWrapper struct {
 	db *badger.DB
 	seq *badger.Sequence
 }
 
-type readClosure func(txn *badger.Txn, companion interface{}) error
-type writeClosure func(txn *badger.Txn, companion interface{}) error
-type iteratorClosure func(it *badger.Iterator, companion interface{}) error
+type readClosure func(txn *badger.Txn, key []byte) error
+type writeClosure func(txn *badger.Txn) error
+type iteratorClosure func(it *badger.Iterator) error
 
-func InitDB() (db DBWrapper, err error) {
+var (
+	db                 DBWrapper
+	ErrUninitializedDB = errors.New("Uninitialized DB")
+)
+
+
+func init() {
 	// Open the Badger database located in the ./feeds directory.
 	// It will be created if it doesn't exist.
 	opts := badger.DefaultOptions
@@ -35,86 +47,203 @@ func InitDB() (db DBWrapper, err error) {
 			db = DBWrapper{ bdb, nil}
 		}
 	}
+}
+
+
+func decodeValue(bs *bytes.Buffer, bdg *badger.Txn, key []byte) (decoder *gob.Decoder, err error) {
+	it, err := bdg.Get(key)
+	decoder = gob.NewDecoder(bs)
+	if err == nil {
+		bts, err := it.ValueCopy(nil)
+		if err == nil {
+			bs.Write(bts)
+		}
+	}
 	return
 }
 
-func (d DBWrapper) readDB(cl readClosure, prm interface{}) (val interface{}, err error) {
-	err = d.db.View(func(txn *badger.Txn) error {
+func addPostToMerge(merge *badger.MergeOperator, post *timeline.Post) {
+	bs := bytes.Buffer{}
+	encoder := gob.NewEncoder(&bs)
+	encoder.Encode(post)
+	merge.Add(bs.Bytes())
+}
+
+
+func mergeNewPost(existing []byte, new []byte) []byte {
+	var posts timeline.Posts
+	bs := bytes.Buffer{}
+	decoder := gob.NewDecoder(&bs)
+	bs.Write(existing)
+	decoder.Decode(posts)
+	posts.Hashes() = append(posts.Hashes(), string(new))
+	bs.Truncate(0)
+	encoder := gob.NewEncoder(&bs)
+	encoder.Encode(posts)
+	return bs.Bytes()
+}
+
+
+func (wrapper DBWrapper) readDB(cl readClosure, key []byte) error {
+	return wrapper.db.View(func(txn *badger.Txn) error {
 		defer txn.Discard()
-		return cl(txn, prm)
+		return cl(txn, key)
 	})
-	val = prm
-	return
 }
 
-func (d DBWrapper) writeDB(cl writeClosure, prm interface{}) (err error) {
-	err = d.db.Update(func(txn *badger.Txn) error {
+func (wrapper DBWrapper) writeDB(cl writeClosure) error {
+	return wrapper.db.Update(func(txn *badger.Txn) error {
 		defer txn.Discard()
-		return cl(txn, prm)
+		return cl(txn)
 	})
-	return
 }
 
-func (d DBWrapper) iteratorDB(cl iteratorClosure, prm interface{}) (val interface{}, err error) {
-	err = d.db.View(func(txn *badger.Txn) error {
+func (wrapper DBWrapper) iteratorDB(cl iteratorClosure) error {
+	return wrapper.db.View(func(txn *badger.Txn) error {
 		defer txn.Discard()
 		opts := badger.DefaultIteratorOptions
-		opts.PrefetchSize = 10
+		opts.PrefetchSize = 100
 		it := txn.NewIterator(opts)
 		defer it.Close()
-		return cl(it, prm)
+		return cl(it)
 	})
-	val = prm
-	return
 }
 
-func (d DBWrapper) keyIteratorDB(cl iteratorClosure, prm interface{}) (val interface{}, err error) {
-	err = d.db.View(func(txn *badger.Txn) error {
+func (wrapper DBWrapper) keyIteratorDB(cl iteratorClosure) error {
+	return wrapper.db.View(func(txn *badger.Txn) error {
 		defer txn.Discard()
 		opts := badger.DefaultIteratorOptions
 		opts.PrefetchValues = false
 		it := txn.NewIterator(opts)
 		defer it.Close()
-		cl(it, prm)
-		return cl(it, prm)
+		return cl(it)
 	})
-	val = prm
-	return
 }
 
 /**
-Should be a merger function.
+	Write a post to the DB and merge its hash
+	into the user's own post feed.
  */
-func (d DBWrapper) WriteToTimeline(new *timeline.Post) error {
-	wr := func (b *badger.Txn, new interface{}) (err error) {
-		post := new.(*timeline.Post)
-		bytes, err := post.AsBinary()
-		if err == nil {
-			b.Set(post.Key(), bytes)
-			b.Commit(nil)
-		}
-		return
+func (wrapper DBWrapper) WriteToTimeline(new *timeline.Post) error {
+	if wrapper.db == nil {
+		merge := wrapper.db.GetMergeOperator([]byte(
+			timeline.GetPosts().Id()),
+			mergeNewPost,
+			200 * time.Millisecond)
+		addPostToMerge(merge, new)
+		merge.Stop()
+		return wrapper.writeDB(func (b *badger.Txn) (err error) {
+			bytes, err := new.AsBinary()
+			if err == nil {
+				b.Set(new.Key(), bytes)
+			}
+			return
+		})
+	} else {
+		return ErrUninitializedDB
 	}
-	return d.writeDB(wr, new)
 }
 
-func (d DBWrapper) WriteTimeline(new *timeline.Timeline) error {
-	wr := func (b *badger.Txn, new interface{}) (err error) {
-		timeline := new.(*timeline.Timeline)
-		bytes, err := timeline.AsBinary()
-		if err == nil {
-			b.Set(timeline.Key(), bytes)
-			b.Commit(nil)
+
+func (wrapper DBWrapper) DumpToSubscribed(key []byte, new []*timeline.Post) error {
+	if wrapper.db == nil {
+		merge := wrapper.db.GetMergeOperator(key, mergeNewPost, 10 * time.Millisecond)
+		for _, v := range new {
+			addPostToMerge(merge, v)
 		}
-		return
+		merge.Stop()
+		return wrapper.writeDB(func(txn *badger.Txn) error {
+			var err error
+			for _, v := range new {
+				b, err := v.AsBinary()
+				if err == nil {
+					txn.Set(v.Key(), b)
+				}
+			}
+			return err
+		})
+	} else {
+		return ErrUninitializedDB
 	}
-	return d.writeDB(wr, new)
 }
 
-func (d DBWrapper) GetNewId() (id uint64) {
-	if d.seq != nil {
+/**
+
+ */
+
+type readStruct struct {
+	fmap map[string]*timeline.Posts
+	subs *peer.Subscribed
+}
+
+/**
+	Read a bunch of keys from subscribed list.
+ */
+func (wrapper DBWrapper) ReadAsFeedMap(subList *peer.Subscribed) (map[string]*timeline.Posts, error) {
+	if wrapper.db != nil {
+		feedMap := make(map[string]*timeline.Posts)
+		rs := &readStruct{
+			feedMap,
+			subList,
+		}
+		err := wrapper.keyIteratorDB(func (b *badger.Iterator) (err error) {
+			var posts *timeline.Posts
+			bs := bytes.Buffer{}
+			decoder := gob.NewDecoder(&bs)
+			var cp []byte
+			for k := range rs.subs.SubList() {
+				b.Seek([]byte(k))
+				it := b.Item()
+				if it == nil {
+					log.Print(badger.ErrRetry)
+				} else {
+					cp = nil
+					cp, err = it.ValueCopy(cp)
+					if err == nil {
+						log.Print(err)
+					} else {
+						bs.Write(cp)
+						decoder.Decode(posts)
+						rs.fmap[posts.Id()] = posts
+						posts = nil
+					}
+				}
+			}
+			return
+		})
+		return feedMap, err
+	} else {
+		return nil, ErrUninitializedDB
+	}
+}
+
+
+func (wrapper DBWrapper) PurgeFeed(feedKey string, posts *timeline.Posts) (err error) {
+	if wrapper.db != nil {
+		wr := func(b *badger.Txn) (err error) {
+			err = b.Delete([]byte(feedKey))
+			if err == nil {
+				for _, v := range posts.Hashes() {
+					//Can't guarantee keys are deleted properly
+					err2 := b.Delete([]byte (v))
+					if err2 != nil {
+						log.Print(err2)
+					}
+				}
+			}
+			return
+		}
+		return wrapper.writeDB(wr)
+	} else {
+		return ErrUninitializedDB
+	}
+}
+
+
+func (wrapper DBWrapper) GetNewId() (id uint64) {
+	if wrapper.seq != nil {
 		var err error
-		id, err = d.seq.Next()
+		id, err = wrapper.seq.Next()
 		if err != nil {
 			id = rand.Uint64()
 		}
@@ -122,5 +251,80 @@ func (d DBWrapper) GetNewId() (id uint64) {
 		id = rand.Uint64()
 	}
 	return
+}
+
+func (wrapper *DBWrapper) GetSubList(b []byte) (sublist *peer.Subscribed, err error) {
+	if wrapper == nil {
+		db.readDB(func(txn *badger.Txn, key []byte) error {
+			bs := &bytes.Buffer{}
+			decoder, err := decodeValue(bs, txn, key)
+			if err == nil {
+				err = decoder.Decode(sublist)
+			}
+			return err
+		}, b)
+		return
+	} else {
+		return nil, ErrUninitializedDB
+	}
+}
+
+func (wrapper *DBWrapper) GetMyKeyPair(b []byte) (id *timeline.OwnId, err error) {
+	if wrapper == nil {
+		db.readDB(func (bdg *badger.Txn, key []byte) (err error)  {
+			bs := &bytes.Buffer{}
+			decoder, err := decodeValue(bs, bdg, key)
+			if err == nil {
+				err = decoder.Decode(id)
+			}
+			return
+		}, b)
+		return
+	} else {
+		return nil, ErrUninitializedDB
+	}
+}
+
+func (wrapper *DBWrapper) GetOwnPosts(b []byte) (posts *timeline.Posts, err error) {
+	if wrapper == nil {
+		db.readDB(func (bdg *badger.Txn, key []byte) (err error)  {
+			bs := &bytes.Buffer{}
+			decoder, err := decodeValue(bs, bdg, key)
+			if err == nil {
+				err = decoder.Decode(posts)
+			}
+			return
+		}, b)
+		return
+	} else {
+		return nil, ErrUninitializedDB
+	}
+}
+
+func (wrapper *DBWrapper) GetMyUser(b []byte) (user string, err error) {
+	if wrapper == nil {
+		var userPtr *[]byte
+		db.readDB(func (bdg *badger.Txn, key []byte) (err error)  {
+			it, err := bdg.Get(key)
+			if err == nil {
+				var bts []byte = nil
+				bts, err = it.ValueCopy(bts)
+				if err == nil {
+					*userPtr = bts
+				}
+			}
+			return
+		}, b)
+		user = string(*userPtr)
+		return
+	} else {
+		return "", ErrUninitializedDB
+	}
+
+}
+
+
+func GetDBWrapper() (*DBWrapper) {
+	return &db
 }
 
--- timeline/timeline.go
+++ timeline/timeline.go
@@ -1,44 +1,130 @@
 package timeline
 
 import (
-	"encoding/binary"
+"crypto"
+"crypto/ecdsa"
+"crypto/elliptic"
+"crypto/rand"
+"encoding/binary"
 
-	"github.com/Seriyin/go-bitaites/db"
-	"github.com/golang/protobuf/proto"
+
+
+
+
+
+"github.com/Seriyin/go-bitaites/db"
+"github.com/dgraph-io/badger"
+
 )
 
-type Timeline struct{
-	timeline *TimelineI
+type timeline struct{
+	feedMap map[string]*Posts
+	timeline []*Post
 }
 
+type Id struct {
+	user string
+	crypto.PublicKey
+}
 
-func (timeline *Timeline) Key() []byte {
+type OwnId struct {
+	Id
+	crypto.PrivateKey
+}
+
+type Posts struct{
+	id string
+	hashes []string
+}
+
+var myid *OwnId
+var own *Posts
+
+var t = &timeline{
+	nil,
+	make([]*Post,40),
+}
+
+func init()  {
+	dbWrapper := db.GetDBWrapper()
+	myuser, err := dbWrapper.GetMyUser([]byte("id-user"))
+	switch err {
+	case nil: myid, err = dbWrapper.GetMyKeyPair([]byte("keypair" + myuser))
+		switch err {
+		case nil:
+			own, err = dbWrapper.GetOwnPosts([]byte("own-posts"))
+			switch err {
+			case nil:
+			case badger.ErrKeyNotFound:
+				//Key not found means generate new Posts
+				own = &Posts{
+					"own-posts",
+					make([]string, 10),
+				}
+			default:
+				panic(err)
+			}
+		case badger.ErrKeyNotFound:
+			//Key not found means generate the keypair.
+			myid = GenerateNewId(myuser)
+		default:
+			panic(err)
+		}
+	case badger.ErrKeyNotFound:
+		panic(err)
+	default:
+		panic(err)
+	}
+}
+
+/**
+	Generate a new Id based on an elliptic P521 curve.
+ */
+func GenerateNewId(user string) *OwnId {
+	pk, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err == nil {
+		return &OwnId{
+			Id{
+				user,
+				pk.Public(),
+			},
+			pk,
+		}
+	} else {
+		return nil
+	}
+}
+
+func (timeline *timeline) Key() []byte {
 	bs := make([]byte, 4)
-	binary.LittleEndian.PutUint32(bs, timeline.timeline.Id)
+	binary.LittleEndian.PutUint32(bs, 0)
 	return bs
 }
 
-func (timeline *Timeline) AsBinary() (ret []byte, err error) {
-	ret, err = proto.Marshal(timeline.timeline)
-	return
+
+func GetTimeline() (*timeline){
+	return t
+}
+
+func GetMyId() (*OwnId) {
+	return myid
+}
+
+func GetPosts() (*Posts) {
+	return own
+}
+
+func (posts *Posts) Id() (string) {
+	return posts.id
+}
+
+func (posts *Posts) Hashes() ([]string) {
+	return posts.hashes
 }
 
 
-func (timeline *Timeline) FromBinary(encoded []byte) (err error) {
-	val := &TimelineI{}
-	err = proto.Unmarshal(encoded, val)
-	if err == nil {
-		timeline.timeline = val
-	}
-	return
-}
-
-func TimelineFromDB(wrapper db.DBWrapper) (*Timeline){
-	return &Timeline{&TimelineI{
-		uint32(wrapper.GetNewId()),
-		map[string]*TimelineI_PostStream{},
-		&TimelineI_PostStream{},
-		struct{}{},
-		make([]byte,0),
-		0}}
+func TimelineKey() (key []byte) {
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[:], 0)
+	return buf[:]
 }
